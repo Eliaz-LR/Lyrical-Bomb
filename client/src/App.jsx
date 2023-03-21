f93feba1e6fb87b3a4a3d5362dd28889f8064832
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-03-21 19:43:19 +0100

    Basic chat working localy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
 import './App.css'
+import io from 'socket.io-client'
+import { useEffect, useState } from 'react'
+
+const socket = io.connect('http://localhost:3001') // Connect to the server running on port 3001
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
+
+  const sendMessage = () => {
+    socket.emit('send_message', {message})
+  };
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageReceived(data.message)
+    })
+  }, [socket])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input placeholder='Message...' onChange={(event) => {
+        setMessage(event.target.value)
+      }}/>
+      <button onClick={sendMessage}>Send message</button>
+      <h1>Received messages :</h1>
+      {messageReceived}
     </div>
-  )
+  );
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -10,11 +10,19 @@ const server = http.createServer(app); // Create server with express app
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Allow only this origin, port 3000 is the default port for react app
+        origin: "http://localhost:5173", // Allow only this origin, port 5173 is the default port for vite dev server
         methods: ["GET", "POST"]
     }
 }); // Create socket.io server
 
+io.on('connection', (socket) => {
+    console.log('user connected: ', socket.id);
+
+    socket.on('send_message', (data) => {
+        console.log(data);
+        socket.broadcast.emit('receive_message', data);
+    })
+});
 
 server.listen(3001, () => {
     console.log('server listening on port 3001');
