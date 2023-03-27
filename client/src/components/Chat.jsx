16cb0ca83334ee462d481a915f2148144df0c71d
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-03-27 15:27:35 +0200

    scroll du chat collé en bas

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -36,13 +36,13 @@ export default function Chat({roomId}) {
     return (
         <div className='flex flex-col overflow-auto h-full md:w-1/2'>
             <h1>Received messages :</h1>
-            <div className='overflow-auto grow border-2 border-b-0 p-1 border-gray-500'>
-                {messagesReceived.map(message => {
+            <div className='flex flex-col-reverse overflow-auto grow border-2 border-b-0 p-1 border-gray-500'>
+                {[...messagesReceived].reverse().map(message => {
                     return <div>{message.user} : {message.content}</div>
                 })}
             </div>
             <form onSubmit={sendMessage} className='flex flex-row border-2 border-gray-500'>
-                <input placeholder='Message...' value={message} className='basis-2/3' onChange={(event) => {
+                <input placeholder='Message...' value={message} className='basis-2/3 p-1' onChange={(event) => {
                 setMessage(event.target.value)
                 }}/>
                 <button type="submit" className='basis-1/3 border-0 border-l-2 border-gray-500 rounded-none'>Send message</button>
