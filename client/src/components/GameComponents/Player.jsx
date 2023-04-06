fd10a0107420f6687e7f27f754f58d97c85eb7af
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-04-06 15:54:46 +0200

    rendering in circles

--- client/src/components/GameComponents/Player.jsx
+++ client/src/components/GameComponents/Player.jsx
@@ -3,6 +3,8 @@ import { useRef, useState, useEffect } from "react"
 
 function Player({username, widthDiv, heightDiv, num, size}) {
     
+    let angle = (num/size) * 2 * Math.PI
+
     const [height, setHeight] = useState(0)
     const [width, setWidth] = useState(0)
     const refPlayer = useRef(null)
@@ -13,8 +15,8 @@ function Player({username, widthDiv, heightDiv, num, size}) {
 
     return (
         <div style={{
-            top: ((heightDiv / 2)- (height / 2)),
-            left: ((widthDiv / 2) - (width / 2)),
+            top: ((heightDiv / 2)- (height / 2))+Math.sin(angle)*heightDiv/4,
+            left: ((widthDiv / 2) - (width / 2))+Math.cos(angle)*widthDiv/4,
         }} ref={refPlayer} className="absolute bg-purple-700">
             <h2>{username} : {num+1}/{size}</h2>
         </div>
