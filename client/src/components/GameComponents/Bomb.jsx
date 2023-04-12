0ccda5927e4fff3f200bc552c3a23aec990e271c
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-04-12 19:20:22 +0200

    Fixed a weird CSS bug on the bomb

--- client/src/components/GameComponents/Bomb.jsx
+++ client/src/components/GameComponents/Bomb.jsx
@@ -2,8 +2,11 @@
 
 export default function Bomb({size = 60, word = 'Bomb'}) {
     return (
-        <div className={`h-${size} w-${size} bg-red-500 rounded-full`}>
-            <span className='text-3xl inline-flex items-center justify-center h-full w-full'>{ word }</span>
+        <div class={`h-${size} w-${size} bg-red-500 rounded-full`}>
+            <span class="text-3xl inline-flex items-center justify-center h-full w-full">{ word }</span>
         </div>
+        // <div class="h-60 w-60 bg-red-500 rounded-full">
+        //     <span class="text-3xl inline-flex items-center justify-center h-full w-full">{ word }</span>
+        // </div>
     )
 }
