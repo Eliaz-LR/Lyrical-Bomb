cf5dffbdc96d16e4a3de24f5748b82c5edefcc76
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-04-24 18:39:14 +0200

    Update Player.jsx

--- client/src/components/GameComponents/Player.jsx
+++ client/src/components/GameComponents/Player.jsx
@@ -15,8 +15,8 @@ function Player({user, isHost, widthDiv, heightDiv, num, size}) {
 
     return (
         <div style={{
-            top: ((heightDiv / 2)- (height / 2))+Math.sin(angle)*heightDiv/4,
-            left: ((widthDiv / 2) - (width / 2))+Math.cos(angle)*widthDiv/4,
+            top: ((heightDiv / 2)- (height / 2))+Math.sin(angle)*heightDiv/3,
+            left: ((widthDiv / 2) - (width / 2))+Math.cos(angle)*widthDiv/3,
         }} ref={refPlayer} className="absolute bg-purple-700 flex flex-col">
             <h2>{user.username} {host}</h2>
             <div className='flex justify-center'>
